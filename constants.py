d6db5d0916c93ef313fa89cb2c57964e2be5e520
Ann Song <gmsong1978@gmail.com>
2024-01-12 12:30:41 -0500

    Constants

--- constants.py
+++ constants.py
@@ -1,1 +1,10 @@
+# DRIVETRAIN
+drivetrain_turn_gear_ratio = 1
+drivetrain_move_gear_ratio = 1
 
+track_width = 0.0
+
+drivetrain_max_vel = 0.0
+drivetrain_max_accel = 0.0
+
+drivetrain_max_angular_vel = 0.0
