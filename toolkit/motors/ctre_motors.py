87d2da45d79cfbbcbf9820b416565f65ce13e146
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-01-12 08:41:37 -0500

    Update ctre_motors.py

--- toolkit/motors/ctre_motors.py
+++ toolkit/motors/ctre_motors.py
@@ -20,7 +20,6 @@ class TalonFX(PIDMotor):
     
     _inverted: bool
     
-    _active_control_mode: controls
     
     def __init__(self, can_id: int, foc: bool = True, inverted: bool = False):
         super().__init__()
@@ -28,18 +27,22 @@ class TalonFX(PIDMotor):
         self._inverted = inverted
         self._foc = foc
         self._can_id = can_id
-        
-    def __set_control_mode(self, mode: controls.DutyCycleOut):
-        self._active_control_mode = mode
+
         
     def get_sensor_position(self) -> rotations:
         return self._motor.get_position()
     
-    def set_target_position(self, pos: radians) -> StatusCode.OK:
-        return self._motor.set_control()
+    def set_target_position(self, pos: radians, arbFF: float = 0.0) -> StatusCode.OK:
+        return self._motor.set_control(controls.MotionMagicExpoDutyCycle(pos * self._inverted, self._foc, arbFF))
+    
+    def set_sensor_position(self, pos: radians) -> StatusCode.OK:
+        return self._motor.set_position(pos * self._inverted)
     
     def set_target_velocity(self, vel: radians_per_second) -> StatusCode.OK:
         return super().set_target_velocity(vel)
     
     def set_raw_output(self, x: float) -> StatusCode.OK:
         self._motor.set_control(controls.DutyCycleOut(x * self._inverted, self._foc))
+        
+    def follow(self, master: TalonFX) -> StatusCode.OK:
+        return self._motor
