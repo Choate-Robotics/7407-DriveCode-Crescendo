3206e014cddb02af4b5a3822e99e60bacaf22e96
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-01-12 20:36:38 -0500

    internal Interface for systems

---
+++ oi/II.py
@@ -0,0 +1,17 @@
+
+from utils import LocalLogger
+
+log = LocalLogger("II")
+
+class OI:
+    
+    
+    
+    @staticmethod
+    def init() -> None:
+        log.info("Initializing II...")
+
+    @staticmethod
+    def map_systems():
+        log.info("Mapping systems...")
+        pass
--- robot.py
+++ robot.py
@@ -11,6 +11,7 @@ import sensors
 import subsystem
 import utils
 from oi.OI import OI
+from oi.II import II
 
 
 class _Robot(wpilib.TimedRobot):
@@ -29,6 +30,9 @@ class _Robot(wpilib.TimedRobot):
         # Initialize Operator Interface
         OI.init()
         OI.map_controls()
+        
+        II.init()
+        II.map_systems()
         period = .03
         commands2.CommandScheduler.getInstance().setPeriod(period)
 
