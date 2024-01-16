d134149e4443d8e2805ee7a6dbc0e89a509759c0
Ann Song <gmsong1978@gmail.com>
2024-01-16 16:16:37 -0500

    Added coords for drive straight

---
+++ autonomous/routines/DRIVE_STRAIGHT/coords.py
@@ -0,0 +1,17 @@
+from units.SI import meters, radians
+
+coord = (meters, meters, radians)
+waypoints = [(meters, meters)]
+path = (coord, waypoints, coord)
+
+blue_team = True
+
+initial: coord = (0, 0, 0)
+
+drive_forward: path = (
+    initial,
+    [],
+    (initial[0] + 1, initial[1]),
+)
+
+
