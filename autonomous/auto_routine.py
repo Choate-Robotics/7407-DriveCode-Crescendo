6301543eacbe092b5d995f2d72a264596c2fdb5a
Carson Ling <carson.ling1102@outlook.com>
2024-01-16 16:07:06 -0500

    initial commit

---
+++ autonomous/auto_routine.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+import commands2
+from commands2 import CommandBase
+from wpimath.geometry import Pose2d
+
+from robot_systems import Robot
+
+
+@dataclass
+class AutoRoutine:
+    """
+    Base auto-routine class.
+
+    :param initial_robot_pose: Initial robot pose.
+    :type initial_robot_pose: Pose2d
+    :param command: Command to run.
+    :type command: CommandBase
+    """
+
+    initial_robot_pose: Pose2d
+    command: CommandBase
+    blue_team: bool = True
+
+    def run(self):
+        """
+        Runs the autonomous routine
+        """
+
+        Robot.drivetrain.gyro.reset_angle(self.initial_robot_pose.rotation().radians())
+        Robot.drivetrain.reset_odometry(self.initial_robot_pose)
+
+        commands2.CommandScheduler.getInstance().schedule(self.command)
