8af17ab63e853674d4b84db83395d0f7c39c35a4
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2024-01-18 16:45:01 -0500

    first draft update() method

--- constants.py
+++ constants.py
@@ -1,4 +1,16 @@
 from wpimath.geometry import Pose2d, Pose3d, Rotation2d, Translation2d
+"""
+c = drag coefficient
+a = projectile area (m^2)
+m = projectile mass (kg)
+rho_air = air density (kg/m^3)
+g = acceleration due to gravity (m/s^2)
+v0 = initial velocity of shooter flywheel (m/s) config
+delta_x = distance from shooter to target (COULD BE IN ODOMETRY) (m)
+y = height of target (COULD BE IN ODOMETRY) (m) const
+tol = tolerance of error in distance to target (m)
+"""
+
 c = 0.47 # drag coefficient
 a = 14*0.0254*2*0.0254 # projectile volume (m^3)
 m = 0.235301 # projectile mass (kg)
