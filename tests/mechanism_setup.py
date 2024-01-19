5dad21ebafedbfbef8213b821a99892483c8d9ad
Matthew Bardoe <mbardoe@gmail.com>
2024-01-19 14:59:37 -0500

    Fix the tests to include a fixture and sometimes the tests pass. Eliminated Mechanism

--- tests/mechanism_setup.py
+++ tests/mechanism_setup.py
@@ -1,118 +1,118 @@
-import pytest
-from toolkit.motors.rev_motors import SparkMax
-from toolkit.subsystem import Subsystem as Subsystem_C
-from rev import CANSparkMax
-
-# to test a subsystem, you need to create a mechanism
-
-# this will override the subsystem's motors and devices with mock versions of them
-
-# this will allow you to test the subsystem without having to worry about the motors and devices, and streamline the process
-
-# if you need to add more motors or devices, you can create a child class of the device and override the methods you need to test
-
-# examples of this are the MockSparkMotor and MockTalonFX classes below
-
-# you can also remove motors and devices by using the remove_parent_variables method in the Mechanism_C class
-
-
-def mechanism(selected_subsystem: Subsystem_C):
-    
-    class MockSparkMotor(SparkMax):
-        
-        class PIDController:
-                
-                def __init__(self, motor: SparkMax):
-                    self._motor = motor
-                    
-                def setReference(self, value, control_type):
-                    if control_type == CANSparkMax.ControlType.kPosition:
-                        self._motor._position = value
-                    elif control_type == CANSparkMax.ControlType.kVelocity:
-                        self._motor._velocity = value
-                    elif control_type == CANSparkMax.ControlType.kVoltage:
-                        self._motor._output = value
-                        
-        class Motor:
-            
-            def __init__(self, motor: SparkMax):
-                self._motor = motor
-                
-            def set(self, value):
-                self._motor._output = value
-                
-            def getAppliedOutput(self):
-                return self._motor._output
-        
-        def __init__(self, motor: SparkMax):
-            super().__init__(motor._can_id, motor._inverted, motor._brushless, motor._config)
-            self._output = 0
-            self._velocity = 0
-            self._position = 0
-            self.pid_controller = self.PIDController(self)
-            self.motor = self.Motor(self)
-            
-        def set_target_velocity(self, vel):
-            self._velocity = vel
-            
-        def get_sensor_velocity(self):
-            return self._velocity
-        
-        def get_target_velocity(self):
-            return self._velocity
-        
-        def set_target_position(self, target):
-            self._position = target
-            
-        def get_sensor_position(self):
-            return self._position
-        
-    class Mechanism_C(selected_subsystem):
-        
-        def __init__(self):
-            super().__init__()
-            
-        def init(self): 
-            
-            self.replace_parent_variables(SparkMax, MockSparkMotor)
-            # replace any motors or devices here
-
-            super().init()
-            
-        def get_parent_variables_by_type(self, type):
-            '''
-            Returns a list of all the variable names of the parent class that are of the specified type
-            '''
-            return  [attr for attr in dir(selected_subsystem) if not callable(getattr(selected_subsystem, attr)) and not attr.startswith("__") and isinstance(getattr(selected_subsystem, attr), type)]
-            
-        def replace_parent_variables(self, type, new_type):
-            '''
-            Replaces all the variables of the parent class that are of the specified type with the new type
-            '''
-            for attr in self.get_parent_variables_by_type(type):
-                super().__setattr__(attr, new_type(getattr(selected_subsystem, attr)))
-                
-        def set_parent_variables(self, type, value):
-            '''
-            Sets all the variables of the parent class that are of the specified type to the specified value
-            
-            NOTE: This is setting the value to a primitive type, so it will not work for objects
-            '''
-            for attr in self.get_parent_variables_by_type(type):
-                super().__setattr__(attr, value)
-                
-        def remove_parent_variables(self, type):
-            '''
-            Removes all the variables of the parent class that are of the specified type
-            '''
-            for attr in self.get_parent_variables_by_type(type):
-                delattr(self, attr)
-            
-        
-        def deconstruct(self):
-            self.remove_parent_variables(SparkMax)
-            # remove any motors or devices here
-    
-    return Mechanism_C()
-        
-        
+# import pytest
+# from toolkit.motors.rev_motors import SparkMax
+# from toolkit.subsystem import Subsystem as Subsystem_C
+# from rev import CANSparkMax
+#
+# # to test a subsystem, you need to create a mechanism
+#
+# # this will override the subsystem's motors and devices with mock versions of them
+#
+# # this will allow you to test the subsystem without having to worry about the motors and devices, and streamline the process
+#
+# # if you need to add more motors or devices, you can create a child class of the device and override the methods you need to test
+#
+# # examples of this are the MockSparkMotor and MockTalonFX classes below
+#
+# # you can also remove motors and devices by using the remove_parent_variables method in the Mechanism_C class
+#
+#
+# def mechanism(selected_subsystem: Subsystem_C):
+#
+#     class MockSparkMotor(SparkMax):
+#
+#         class PIDController:
+#
+#                 def __init__(self, motor: SparkMax):
+#                     self._motor = motor
+#
+#                 def setReference(self, value, control_type):
+#                     if control_type == CANSparkMax.ControlType.kPosition:
+#                         self._motor._position = value
+#                     elif control_type == CANSparkMax.ControlType.kVelocity:
+#                         self._motor._velocity = value
+#                     elif control_type == CANSparkMax.ControlType.kVoltage:
+#                         self._motor._output = value
+#
+#         class Motor:
+#
+#             def __init__(self, motor: SparkMax):
+#                 self._motor = motor
+#
+#             def set(self, value):
+#                 self._motor._output = value
+#
+#             def getAppliedOutput(self):
+#                 return self._motor._output
+#
+#         def __init__(self, motor: SparkMax):
+#             super().__init__(motor._can_id, motor._inverted, motor._brushless, motor._config)
+#             self._output = 0
+#             self._velocity = 0
+#             self._position = 0
+#             self.pid_controller = self.PIDController(self)
+#             self.motor = self.Motor(self)
+#
+#         def set_target_velocity(self, vel):
+#             self._velocity = vel
+#
+#         def get_sensor_velocity(self):
+#             return self._velocity
+#
+#         def get_target_velocity(self):
+#             return self._velocity
+#
+#         def set_target_position(self, target):
+#             self._position = target
+#
+#         def get_sensor_position(self):
+#             return self._position
+#
+#     class Mechanism_C(selected_subsystem):
+#
+#         def __init__(self):
+#             super().__init__()
+#
+#         def init(self):
+#
+#             self.replace_parent_variables(SparkMax, MockSparkMotor)
+#             # replace any motors or devices here
+#
+#             super().init()
+#
+#         def get_parent_variables_by_type(self, type):
+#             '''
+#             Returns a list of all the variable names of the parent class that are of the specified type
+#             '''
+#             return  [attr for attr in dir(selected_subsystem) if not callable(getattr(selected_subsystem, attr)) and not attr.startswith("__") and isinstance(getattr(selected_subsystem, attr), type)]
+#
+#         def replace_parent_variables(self, type, new_type):
+#             '''
+#             Replaces all the variables of the parent class that are of the specified type with the new type
+#             '''
+#             for attr in self.get_parent_variables_by_type(type):
+#                 super().__setattr__(attr, new_type(getattr(selected_subsystem, attr)))
+#
+#         def set_parent_variables(self, type, value):
+#             '''
+#             Sets all the variables of the parent class that are of the specified type to the specified value
+#
+#             NOTE: This is setting the value to a primitive type, so it will not work for objects
+#             '''
+#             for attr in self.get_parent_variables_by_type(type):
+#                 super().__setattr__(attr, value)
+#
+#         def remove_parent_variables(self, type):
+#             '''
+#             Removes all the variables of the parent class that are of the specified type
+#             '''
+#             for attr in self.get_parent_variables_by_type(type):
+#                 delattr(self, attr)
+#
+#
+#         def deconstruct(self):
+#             self.remove_parent_variables(SparkMax)
+#             # remove any motors or devices here
+#
+#     return Mechanism_C()
+#
+#
--- tests/subsystems/test_elevator.py
+++ tests/subsystems/test_elevator.py
@@ -1,33 +1,40 @@
 import pytest
 import rev
 from pytest import MonkeyPatch
+from toolkit.motors.rev_motors import SparkMax, SparkMaxConfig
 
 import config
 import constants
 from subsystem import Elevator
 from unittest.mock import MagicMock
 
+@pytest.fixture()
+def elevator() -> Elevator:
+    # Create a drivetrain, but it has mock
+    # classes for its dependencies
+    my_elevator = Elevator()
+    my_elevator.motor_extend: SparkMax = MagicMock()
+    my_elevator.motor_extend.motor = MagicMock()
+    my_elevator.motor_extend_follower: SparkMax = MagicMock()
+    my_elevator.encoder = MagicMock()
+    my_elevator.motor_extend.pid_controller = MagicMock()
+    #my_elevator.init()
+    return my_elevator
 
-def test_elevator_dunder_init():
-    elevator = Elevator()
+def test_elevator_dunder_init(elevator: Elevator):
+
     elevator.init()
+
     # elevator.encoder = MagicMock()
     # elevator.motor_extend = MagicMock()
     assert elevator.zeroed == False
-    assert elevator.encoder != False
+    #assert elevator.encoder != False
     assert elevator.motor_extend != False
-
-
-def test_elevator_init():
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.encoder = MagicMock()
-    elevator.init()
-
     elevator.motor_extend.init.assert_called()
     elevator.motor_extend.get_abs.assert_called()
     elevator.motor_extend.motor.setClosedLoopRampRate.assert_called()
+
+
 
 
 @pytest.mark.parametrize(
@@ -39,18 +46,12 @@ def test_elevator_init():
         (0),
     ],
 )
-def test_set_length(test_input):
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
-    elevator.init()
+def test_set_length(test_input, elevator: Elevator):
+    #elevator.init()
     elevator.set_length(test_input)
-    elevator.motor_extend.pid_controller.setReference.assert_called_with(
+    elevator.motor_extend.set_target_position.assert_called_with(
         (test_input * constants.elevator_gear_ratio)
-        / constants.elevator_driver_gear_circumfrance,
-        rev.CANSparkMax.ControlType.kPosition,
-        arbFeedforward=config.elevator_feed_forward,
+        / constants.elevator_driver_gear_circumfrance
     )
 
 
@@ -63,14 +64,10 @@ def test_set_length(test_input):
         (0),
     ],
 )
-def test_get_length(test_input):
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
-    elevator.init()
-    elevator.get_length()
-    elevator.motor_extend.get_sensor_position.assert_called()
+def test_get_length(test_input, elevator: Elevator):
+
+    #elevator.get_length()
+    #elevator.motor_extend.get_sensor_position.assert_called()
     elevator.motor_extend.get_sensor_position.return_value = test_input
     assert (
         elevator.get_length()
@@ -82,68 +79,65 @@ def test_get_length(test_input):
 @pytest.mark.parametrize(
     "test_input,actual", [(0.2, 0.2), (3, 1), (1, 1), (0, 0), (-0.2, 0)]
 )
-def test_set_motor_position(test_input, actual):
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
-    elevator.init()
+def test_set_motor_position(test_input, actual, elevator: Elevator):
     elevator.set_motor_position(test_input)
     elevator.motor_extend.set_sensor_position.assert_called_with(
         actual * config.elevator_max_rotation
     )
 
-
-def test_zero():
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
-    elevator.init()
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (1),
+        (10),
+        (1000),
+        (0),
+    ],
+)
+def test_zero(test_input, elevator: Elevator):
+    elevator.encoder.getPosition.return_value = test_input
     elevator.zero()
     elevator.motor_extend.set_sensor_position.assert_called_with(
-        elevator.encoder.getPosition() * constants.elevator_gear_ratio
-    )
+        test_input * constants.elevator_gear_ratio)
     assert elevator.zeroed == True
 
-
-def test_set_voltage():
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
-    elevator.init()
-    elevator.set_voltage(1)
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (0.5),
+        (0.2),
+        (0.1),
+        (0),
+    ],
+)
+def test_set_voltage(test_input, elevator: Elevator):
+    elevator.set_voltage(test_input)
     elevator.motor_extend.pid_controller.setReference.assert_called_with(
-        1, rev.CANSparkMax.ControlType.kVoltage
+        test_input, rev.CANSparkMax.ControlType.kVoltage
     )
 
 
-def test_get_voltage():
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
+def test_get_voltage(elevator: Elevator):
     elevator.motor_extend.motor.getAppliedOutput.return_value = 1
-    elevator.init()
     value = elevator.get_voltage()
     elevator.motor_extend.motor.getAppliedOutput.assert_called()
     assert value == 1
 
-
-def test_stop(monkeypatch: MonkeyPatch):
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (0.5),
+        (1.2),
+        (0.1),
+        (0),
+    ],
+)
+def test_stop(test_input, elevator: Elevator, monkeypatch: MonkeyPatch):
     def mock_get_length(self):
-        return 1
+        return test_input
 
-    elevator = Elevator()
-    elevator.motor_extend = MagicMock()
-    elevator.motor_extend.motor = MagicMock()
-    elevator.motor_extend.pid_controller = MagicMock()
     monkeypatch.setattr(Elevator, "get_length", mock_get_length)
-    elevator.init()
     elevator.stop()
-    elevator.motor_extend.pid_controller.setReference.assert_called_with(
-        (1 * constants.elevator_gear_ratio)
-        / constants.elevator_driver_gear_circumfrance,
-        rev.CANSparkMax.ControlType.kPosition,
-        arbFeedforward=config.elevator_feed_forward
-    )
+    elevator.motor_extend.set_target_position.assert_called_with(
+        (test_input * constants.elevator_gear_ratio)
+        / constants.elevator_driver_gear_circumfrance)
