2271c554b937f8b59af80ed24b28292a20f2796a
Matthew Bardoe <mbardoe@gmail.com>
2024-01-19 13:49:52 -0500

    Redid the test_elevator.py to use MagicMock and Monkeypatch.

--- tests/subsystems/test_elevator.py
+++ tests/subsystems/test_elevator.py
@@ -1,66 +1,149 @@
 import pytest
+import rev
+from pytest import MonkeyPatch
+
 import config
-import time
+import constants
 from subsystem import Elevator
-from tests import mechanism
-# class ElevatorMock(Elevator):
-
-#     length = 0.0
-#     voltage = 0.0
-
-#     def __init__(self):
-#         super().__init__()
-#         self.zeroed = False
-#         self.encoder = False
-#         self.motor_extend = False
-
-#     def init(self):
-#         self.zeroed = False
-#         self.encoder = True
-#         self.motor_extend = True
-
-#     def set_length(self, length: float):
-#         self.length = length
-
-#     def get_length(self):
-#         return self.length
-    
-#     def set_voltage(self, voltage: float):
-#         self.voltage = voltage
-
-#     def get_voltage(self):
-#         return self.voltage
-    
-#     def zero(self):
-#         self.zeroed = True
-#         self.length = 0.0
-
-@pytest.fixture(scope='module')
-def elevator():
-    m = mechanism(Elevator)
-    yield m
+from unittest.mock import MagicMock
 
 
-def test_elevator_init(elevator):
+def test_elevator_dunder_init():
+    elevator = Elevator()
     elevator.init()
-    time.sleep(1)
+    # elevator.encoder = MagicMock()
+    # elevator.motor_extend = MagicMock()
     assert elevator.zeroed == False
     assert elevator.encoder != False
     assert elevator.motor_extend != False
 
-def test_elevator_len(elevator):
-    elevator.set_length(0.5)
-    time.sleep(1)
-    assert round(elevator.get_length(), 1) == 0.5
 
-def test_elevator_zero(elevator):
+def test_elevator_init():
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.encoder = MagicMock()
+    elevator.init()
+
+    elevator.motor_extend.init.assert_called()
+    elevator.motor_extend.get_abs.assert_called()
+    elevator.motor_extend.motor.setClosedLoopRampRate.assert_called()
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (0.5),
+        (0.2),
+        (0.1),
+        (0),
+    ],
+)
+def test_set_length(test_input):
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    elevator.init()
+    elevator.set_length(test_input)
+    elevator.motor_extend.pid_controller.setReference.assert_called_with(
+        (test_input * constants.elevator_gear_ratio)
+        / constants.elevator_driver_gear_circumfrance,
+        rev.CANSparkMax.ControlType.kPosition,
+        arbFeedforward=config.elevator_feed_forward,
+    )
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (1),
+        (10),
+        (1000),
+        (0),
+    ],
+)
+def test_get_length(test_input):
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    elevator.init()
+    elevator.get_length()
+    elevator.motor_extend.get_sensor_position.assert_called()
+    elevator.motor_extend.get_sensor_position.return_value = test_input
+    assert (
+        elevator.get_length()
+        == (test_input / constants.elevator_gear_ratio)
+        * constants.elevator_driver_gear_circumfrance
+    )
+
+
+@pytest.mark.parametrize(
+    "test_input,actual", [(0.2, 0.2), (3, 1), (1, 1), (0, 0), (-0.2, 0)]
+)
+def test_set_motor_position(test_input, actual):
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    elevator.init()
+    elevator.set_motor_position(test_input)
+    elevator.motor_extend.set_sensor_position.assert_called_with(
+        actual * config.elevator_max_rotation
+    )
+
+
+def test_zero():
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    elevator.init()
     elevator.zero()
-    time.sleep(1)
+    elevator.motor_extend.set_sensor_position.assert_called_with(
+        elevator.encoder.getPosition() * constants.elevator_gear_ratio
+    )
     assert elevator.zeroed == True
-    print(elevator.get_length()) # REM IF PASSED
-    assert round(elevator.get_length(), 1) == 0.0
 
-def test_set_voltage(elevator):
-    elevator.set_voltage(6)
-    time.sleep(1)
-    assert round(elevator.get_voltage()) == 6
+
+def test_set_voltage():
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    elevator.init()
+    elevator.set_voltage(1)
+    elevator.motor_extend.pid_controller.setReference.assert_called_with(
+        1, rev.CANSparkMax.ControlType.kVoltage
+    )
+
+
+def test_get_voltage():
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.motor.getAppliedOutput.return_value = 1
+    elevator.init()
+    value = elevator.get_voltage()
+    elevator.motor_extend.motor.getAppliedOutput.assert_called()
+    assert value == 1
+
+
+def test_stop(monkeypatch: MonkeyPatch):
+    def mock_get_length(self):
+        return 1
+
+    elevator = Elevator()
+    elevator.motor_extend = MagicMock()
+    elevator.motor_extend.motor = MagicMock()
+    elevator.motor_extend.pid_controller = MagicMock()
+    monkeypatch.setattr(Elevator, "get_length", mock_get_length)
+    elevator.init()
+    elevator.stop()
+    elevator.motor_extend.pid_controller.setReference.assert_called_with(
+        (1 * constants.elevator_gear_ratio)
+        / constants.elevator_driver_gear_circumfrance,
+        rev.CANSparkMax.ControlType.kPosition,
+        arbFeedforward=config.elevator_feed_forward
+    )
