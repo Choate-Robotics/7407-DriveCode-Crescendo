469f0d42cfd868f23e974e4b7acec6bcea0a86dd
Ann Song <gmsong1978@gmail.com>
2024-01-23 17:11:06 -0500

    Working square

--- autonomous/routines/SQUARE/coords.py
+++ autonomous/routines/SQUARE/coords.py
@@ -12,23 +12,23 @@ initial: coord = (0, 0, 0)
 drive_forward: path = (
     initial,
     [],
-    (initial[0] + 1.5, initial[1], 0),
+    (initial[0] + 1, initial[1], -math.pi/2),
 )
 
 drive_left: path = (
     drive_forward[2],
     [],
-    (initial[0] + 1.5, initial[1] + 1.5, -math.pi/2)
+    (initial[0] + 1, initial[1] + 1, math.pi)
 )
 
 drive_back: path = (
     drive_left[2],
     [],
-    (initial[0], initial[1] + 1.5, -math.pi)
+    (initial[0], initial[1] + 1, -math.pi * 3 / 2)
 )
 
 drive_right: path = (
     drive_back[2],
     [],
-    (initial[0], initial[1], -math.pi*3/2)
+    (initial[0], initial[1], 0)
 )
