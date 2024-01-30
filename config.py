41d7a3de2dc80e0695a31d9dd3e324e5aefd414c
Matthew Bardoe <mbardoe@gmail.com>
2024-01-30 00:36:30 -0500

    Trajectory Calculator on shooter angle working and tested. Had to move some things from config to constants to get it to compile. Elevator code had error

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
+# from dataclasses import dataclass
 from enum import Enum
-from wpimath.geometry import Pose3d, Rotation3d
-from dataclasses import dataclass
-from wpilib import AnalogEncoder, DigitalInput
-from toolkit.motors import SparkMaxConfig
-comp_bot: DigitalInput = DigitalInput(0) # if true, we are using the practice bot (we will put a jumper on the DIO port)
 
-from units.SI import (
-    inches_to_meters,
-    meters,
-    meters_per_second,
-    meters_per_second_squared,
-    radians,
-)
+from wpilib import AnalogEncoder, DigitalInput
+from wpimath.geometry import Pose3d, Rotation3d
+
+from toolkit.motors import SparkMaxConfig
+
+comp_bot: DigitalInput = DigitalInput(
+    0
+)  # if true, we are using the practice bot (we will put a jumper on the DIO port)
+
+# from units.SI import (
+#     inches_to_meters,
+#     meters,
+#     meters_per_second,
+#     meters_per_second_squared,
+#     radians,
+# )
 
 DEBUG_MODE: bool = True
 # MAKE SURE TO MAKE THIS FALSE FOR COMPETITION
@@ -35,23 +40,22 @@ LOG_FILE_LEVEL: int = 1
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
-
 period: float = 0.03  # seconds
 
 
-#intake
-inner_intake_id = 0 #placeholder
-outer_intake_front_id = 1 #placeholder
-outer_intake_back_id = 2 #placeholder
-intake_beam_break_channel = 1 #placeholder
+# intake
+inner_intake_id = 0  # placeholder
+outer_intake_front_id = 1  # placeholder
+outer_intake_back_id = 2  # placeholder
+intake_beam_break_channel = 1  # placeholder
 
-intake_inner_speed = 0.25 #placeholder
-intake_outer_speed = 0.5 #placeholder
-intake_outer_idle_speed = .25 #placeholder
+intake_inner_speed = 0.25  # placeholder
+intake_outer_speed = 0.5  # placeholder
+intake_outer_idle_speed = 0.25  # placeholder
 
-intake_timeout = 5 #placeholder
-intake_roller_current_limit = 1 #placeholder
-intake_sensor_debounce = 0.2 #placeholder
+intake_timeout = 5  # placeholder
+intake_roller_current_limit = 1  # placeholder
+intake_sensor_debounce = 0.2  # placeholder
 
 # elevator
 elevator_can_id: int = 73  # TODO: PLACEHOLDER
@@ -60,65 +64,40 @@ elevator_ramp_rate: float = 1.0  # TODO: PLACEHOLDER
 elevator_max_rotation: float = 1.0  # TODO: PLACEHOLDER
 elevator_auto_position: float = 1.0  # TODO: PLACEHOLDER
 elevator_feed_forward: float = 0.65  # TODO: PLACEHOLDER
-  
+
 # Wrist
 wrist_motor_id = 30
-WRIST_CONFIG = SparkMaxConfig(.1, 0, 0.003, 0.00015, (-.5, .5))
+WRIST_CONFIG = SparkMaxConfig(0.1, 0, 0.003, 0.00015, (-0.5, 0.5))
 feed_motor_id = 50
-FEED_CONFIG = SparkMaxConfig(.1, 0, 0.003, 0.00015, (-.5, .5))
+FEED_CONFIG = SparkMaxConfig(0.1, 0, 0.003, 0.00015, (-0.5, 0.5))
 feeder_velocity = 132
-
 
 
 # LEDS
 def KRainbow():
-    return {
-        'type': 2
-    }
+    return {"type": 2}
 
 
 class Type:
-
     def KStatic(r, g, b):
-        return {
-            'type': 1,
-            'color': {
-                'r': r,
-                'g': g,
-                'b': b
-            }
-        }
+        return {"type": 1, "color": {"r": r, "g": g, "b": b}}
 
     def KTrack(r1, g1, b1, r2, g2, b2):
         return {
-            'type': 3,
-            'color': {
-                'r1': r1,
-                'g1': g1,
-                'b1': b1,
-                'r2': r2,
-                'g2': g2,
-                'b2': b2
-            }
+            "type": 3,
+            "color": {"r1": r1, "g1": g1, "b1": b1, "r2": r2, "g2": g2, "b2": b2},
         }
 
     def KBlink(r, g, b):
-        return {
-            'type': 4,
-            'color': {
-                'r': r,
-                'g': g,
-                'b': b
-            }
-        }
+        return {"type": 4, "color": {"r": r, "g": g, "b": b}}
 
     def KLadder(typeA, typeB, percent, speed):
         return {
-            'type': 5,
-            'percent': percent,  # 0-1
-            'typeA': typeA,
-            'typeB': typeB,
-            'speed': speed
+            "type": 5,
+            "percent": percent,  # 0-1
+            "typeA": typeA,
+            "typeB": typeB,
+            "speed": speed,
         }
 
 
@@ -139,10 +118,10 @@ class LimelightPipeline:
 
 
 limelight_led_mode = {
-    'pipline_default': 0,
-    'force_off': 1,
-    'force_blink': 2,
-    'force_on': 3
+    "pipline_default": 0,
+    "force_off": 1,
+    "force_blink": 2,
+    "force_on": 3,
 }
 
 
@@ -175,8 +154,6 @@ back_right_encoder_zeroed_pos = 0.260 if comp_bot.get() else 0.0
 driver_centric: bool = True
 drivetrain_reversed: bool = False
 
-#pathing
-post_avoidance_distance = 0.5 * meters
 
 # Gyro
 gyro_id = 20
@@ -184,18 +161,17 @@ gyro_id = 20
 # Elevator
 elevator_moving = False
 
-"""
-c = drag coefficient
-a = projectile area (m^2)
-m = projectile mass (kg)
-rho_air = air density (kg/m^3)
-g = acceleration due to gravity (m/s^2)
-v0 = initial velocity of shooter flywheel (m/s) config
-delta_x = distance from shooter to target (COULD BE IN ODOMETRY) (m)
-y = height of target (COULD BE IN ODOMETRY) (m) const
-tol = tolerance of error in distance to target (m)
-"""
-v0_flywheel = 15
-shooter_tol = 0.1
-max_sim_times = 100
 
+# c = drag coefficient
+# a = projectile area (m^2)
+# m = projectile mass (kg)
+# rho_air = air density (kg/m^3)
+# g = acceleration due to gravity (m/s^2)
+# v0 = initial velocity of shooter flywheel (m/s) config
+# delta_x = distance from shooter to target (COULD BE IN ODOMETRY) (m)
+# y = height of target (COULD BE IN ODOMETRY) (m) const
+# tol = tolerance of error in distance to target (m)
+
+v0_flywheel = 15
+shooter_tol = 0.001
+max_sim_times = 100
--- sensors/trajectory_calc.py
+++ sensors/trajectory_calc.py
@@ -9,6 +9,7 @@ import config
 import constants
 from sensors.field_odometry import FieldOdometry
 from subsystem import Elevator
+from toolkit.utils.toolkit_math import NumericalIntegration, extrapolate
 
 # from scipy.integrate import solve_ivp
 # from wpimath.geometry import Pose2d, Pose3d, Rotation2d, Translation2d
@@ -36,6 +37,7 @@ class TrajectoryCalculator:
         self.shoot_angle = 0
         self.base_angle = 0
         self.elevator = elevator
+        self.numerical_integration = NumericalIntegration()
 
     def calculate_angle_no_air(self, distance_to_target: float, delta_z) -> float:
         """
@@ -61,9 +63,11 @@ class TrajectoryCalculator:
         :return: target angle
         """
         self.distance_to_target = (
-            self.odometry.getPose().translation().distance(self.speaker)
+            self.odometry.getPose().translation().distance(self.speaker.translation())
         )
-        self.delta_z = self.speaker_z - self.elevator.get_height()
+
+        self.delta_z = self.speaker_z - self.elevator.get_length()
+        print("in program:" + str(self.delta_z))
         theta_1 = self.calculate_angle_no_air(self.distance_to_target, self.delta_z)
         theta_2 = theta_1 + np.radians(1)
         z_1 = self.run_sim(theta_1)
@@ -73,15 +77,16 @@ class TrajectoryCalculator:
         correction_angle = z_goal_error * z_to_angle_conversion
         for i in range(config.max_sim_times):
             theta_1 = theta_2
-            theta_2 = theta_1 + correction_angle
+            theta_2 = theta_2 + correction_angle
             z_1 = z_2
             z_2 = self.run_sim(theta_2)
-            z_error = self.delta_z - z_2
+            z_goal_error = self.delta_z - z_2
             z_to_angle_conversion = (theta_2 - theta_1) / (z_2 - z_1)
-            if abs(z_error) < config.shooter_tol:
+            print(z_goal_error, theta_2, self.delta_z)
+            if abs(z_goal_error) < config.shooter_tol:
                 self.shoot_angle = theta_2
                 return theta_2
-            correction_angle = z_error * z_to_angle_conversion
+            correction_angle = z_goal_error * z_to_angle_conversion
 
     def update_base(self):
         """
@@ -101,34 +106,27 @@ class TrajectoryCalculator:
     def run_sim(self, shooter_theta):
         def hit_target(t, u):
             # We've hit the target if the distance to target is 0.
-            return self.distance_to_target - u[0]
+            return u[0] > self.distance_to_target
 
-        # u0 = (
-        #     0,
-        #     config.v0_flywheel * np.cos(shooter_theta),
-        #     0.0,
-        #     config.v0_flywheel * np.sin(shooter_theta),
-        # )
-        # t0, tf = 0, 60
+        u0 = (
+            0,
+            config.v0_flywheel * np.cos(shooter_theta),
+            0.0,
+            config.v0_flywheel * np.sin(shooter_theta),
+        )
+        t0, tf = 0, 60
         # Stop the integration when we hit the target.
-        hit_target.terminal = True
-        # We must be moving downwards (don't stop before we begin moving upwards!)
-        hit_target.direction = -1
-        # WILL NEED ANOTHER METHOD TO SOLVE
-        # sim_solution_n = solve_ivp(
-        #     self.deriv,
-        #     (t0, tf),
-        #     u0,
-        #     method="DOP853",
-        #     dense_output=True,
-        #     events=(hit_target),
-        # )
-        # print(sim_solution)
-        # t = np.linspace(0, sim_solution_n.t_events[0][0], 100)
-        # sim_solution = sim_solution_n.sol(t)
-        # x, z = sim_solution[0], sim_solution[2]
-        z = 1
-        return z
+        t, y = self.numerical_integration.adaptive_rk4(
+            self.deriv, u0, t0, tf, 0.001, 1e-9, hit_target
+        )
+
+        # y[-2][0] is the penultimate x value
+        # y[-2][2] is the penultimate z value
+        # y[-1][0] is the final x value
+        # y[-1][2] is the final z value
+        return extrapolate(
+            self.distance_to_target, y[-2][0], y[-2][2], y[-1][0], y[-1][2]
+        )
 
     def get_theta(self) -> float:
         """
@@ -141,4 +139,4 @@ class TrajectoryCalculator:
         speed = np.hypot(xdot, zdot)
         xdotdot = -self.k / constants.m * speed * xdot
         zdotdot = -self.k / constants.m * speed * zdot - constants.g
-        return xdot, xdotdot, zdot, zdotdot
+        return np.array([xdot, xdotdot, zdot, zdotdot])
