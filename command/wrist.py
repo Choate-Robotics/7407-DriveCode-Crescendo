460813616496e6a497917dda89578c2ef063bd75
cyrusnaficy <cyrus.naficy@gmail.com>
2024-02-16 13:39:48 -0500

    init

--- command/wrist.py
+++ command/wrist.py
@@ -1,13 +1,8 @@
 import utils
-import constants
-import wpilib
-import config
-from oi.keymap import Controllers
 from toolkit.command import SubsystemCommand
 from subsystem import Wrist
-from commands2 import SequentialCommandGroup
 from units.SI import radians
-import math
+from sensors.trajectory_calc import TrajectoryCalculator
 
 class ZeroWrist(SubsystemCommand[Wrist]):
 
@@ -122,3 +117,20 @@ class PassNote(SubsystemCommand[Wrist]):
         else:
             # utils.LocalLogger.debug("Note transferred")
             self.subsystem.note_staged = False
+
+class AimWrist(SubsystemCommand[Wrist]):
+    
+    def __init__(self, subsystem: Wrist, trajectory: TrajectoryCalculator):
+        super().__init__(subsystem)
+        self.subsystem = subsystem
+        self.trajectory = trajectory
+
+    def initialize(self):
+        self.get_pos = self.trajectory.update_shooter()
+        self.subsystem.set_wrist_angle(self.get_pos)
+
+    def execute(self):
+        pass
+
+    def isFinished(self):
+        return self.subsystem.is_at_angle(self.get_pos)
