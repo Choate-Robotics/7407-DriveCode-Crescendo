b1316e3c1d1059f31bb96ed4d9004b05e9cd4d09
Matthew Bardoe <mbardoe@gmail.com>
2024-02-18 13:43:57 -0500

    Fixed tests, now using voltage.

--- tests/subsystems/test_wrist.py
+++ tests/subsystems/test_wrist.py
@@ -40,7 +40,8 @@ def test_wrist_set_wrist_angle(test_input, disabled, wrist: Wrist):
         wrist.wrist_motor.set_target_position.assert_not_called()
     else:
         wrist.wrist_motor.set_target_position.assert_called_with(
-            test_input / (pi * 2) * constants.wrist_gear_ratio, config.wrist_flat_ff * math.cos(test_input)
+            test_input / (pi * 2) * constants.wrist_gear_ratio,
+            config.wrist_flat_ff * math.cos(test_input),
         )
 
 
@@ -102,9 +103,9 @@ def test_feed_in(test_input, wrist: Wrist):
     wrist.feed_disabled = test_input
     wrist.feed_in()
     if test_input:
-        wrist.feed_motor.set_raw_output.assert_not_called()
+        wrist.feed_motor.set_target_voltage.assert_not_called()
     else:
-        wrist.feed_motor.set_raw_output.assert_called_with(config.feeder_velocity)
+        wrist.feed_motor.set_target_voltage.assert_called_with(config.feeder_voltage)
 
 
 @pytest.mark.parametrize(
@@ -115,6 +116,6 @@ def test_feed_out(test_input, wrist: Wrist):
     wrist.feed_disabled = test_input
     wrist.feed_out()
     if test_input:
-        wrist.feed_motor.set_raw_output.assert_not_called()
+        wrist.feed_motor.set_target_voltage.assert_not_called()
     else:
-        wrist.feed_motor.set_raw_output.assert_called_with(-config.feeder_velocity)
+        wrist.feed_motor.set_target_voltage.assert_called_with(-config.feeder_voltage)
