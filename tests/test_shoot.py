b81b19976a18f731501720cca7b40477f6fead32
Matthew Bardoe <mbardoe@gmail.com>
2024-02-18 13:36:09 -0500

    Fixed tests

--- tests/test_shoot.py
+++ tests/test_shoot.py
@@ -23,34 +23,36 @@ def trajectory_calc():
     "odometry, expected_angle",
     [
         (
-            Pose2d(4, 2, Rotation2d(0)),
-            0.3283533945463254,
+            Pose2d(2, 2, Rotation2d(0)),
+            0.755503671722051,
         ),
         (
-            Pose2d(3, 0, Rotation2d(0)),
-            0.2952840561723981,
+            Pose2d(3, 2, Rotation2d(0)),
+            0.8095379799856197,
         ),
         (
             Pose2d(2, 2, Rotation2d(0)),
-            0.3938190353882768,
+            0.755503671722051,
         ),
         (
             Pose2d(1, 4, Rotation2d(0)),
-            0.719368713509958,
+            0.8567503782414659,
         ),
         (
             Pose2d(0, 2, Rotation2d(0)),
-            0.4264439888832886,
+            0.7372533576437418,
         ),
     ],
 )
 def test_update_no_air(
     odometry, expected_angle, trajectory_calc, monkeypatch: MonkeyPatch
 ):
+    monkeypatch.setattr(constants, "c", 0.47)
+    monkeypatch.setattr(constants, "g", 9.81)
     monkeypatch.setattr(trajectory_calc.odometry, "getPose", lambda: odometry)
     monkeypatch.setattr(trajectory_calc.elevator, "get_length", lambda: 0.0)
     # print(f"target: {odometry}", sys.stdout)
-
+    # print(constants.c)
     assert trajectory_calc.calculate_angle_no_air() == pytest.approx(
         expected_angle, abs=math.radians(1)
     )
@@ -62,17 +64,17 @@ def test_update_no_air(
         (
             0.3,
             Pose2d(4, 2, Rotation2d(0)),
-            0.9051642266432294,
+            0.7878720297645241,
         ),
         (
             0.56,
             Pose2d(3, 7, Rotation2d(0)),
-            1.6462894479801808,
+            1.6108861283837188,
         ),
         (
             0.9,
             Pose2d(2, 2, Rotation2d(0)),
-            4.02108901258058,
+            3.8380380155817693,
         ),
         (
             0.52,
@@ -82,7 +84,7 @@ def test_update_no_air(
         (
             1.0031,
             Pose2d(3, 9, Rotation2d(0)),
-            5.274972441317853,
+            4.873360734418127,
         ),
     ],
 )
@@ -108,8 +110,8 @@ def test_run_sim(
         (Pose2d(1, 6.8, Rotation2d(0)), 0.8168519268236714),
         (Pose2d(2, 5.1, Rotation2d(0)), 0.717954626335556),
         (Pose2d(0.7, 5.54, Rotation2d(0)), 1.2705172634003417),
-        (Pose2d(7, 4, Rotation2d(0)), 0.38871749175127635),
-        (Pose2d(4, 2, Rotation2d(0)), 0.3880263622269912),
+        (Pose2d(7, 4, Rotation2d(0)), 0.43793129522068697),
+        (Pose2d(4, 2, Rotation2d(0)), 0.42853615436478704),
     ],
 )
 def test_update_shooter(
