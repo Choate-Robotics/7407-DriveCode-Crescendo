e20b05b8d4ec56e749b3acdee44f739e2524461d
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-02-19 23:28:45 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -26,8 +26,6 @@ a = 14 * 0.0254 * 2 * 0.0254  # projectile surface area (m^2)
 m = 0.235301  # projectile mass (kg)
 rho_air = 1.28  # air density (kg/m^3)
 g = 9.8  # acceleration due to gravity (m/s^2)
-speaker_z = 1.7  # height of target (m) CHANGE THIS
-speaker_location = Translation2d(0, 0)
 
 # Drivetrain
 drivetrain_turn_gear_ratio = 150 / 7
