f19ad1d310cf1cc7f20fe596991853dcb75d7e22
e-bauman <ebauman26@choate.edu>
2024-02-29 16:29:19 -0500

    worked on far right midline auto (not working)

--- autonomous/routines/MIDLINE_NOTES/coords.py
+++ autonomous/routines/MIDLINE_NOTES/coords.py
@@ -5,11 +5,10 @@ from wpimath.geometry import Translation2d
 # from constants.FieldPos import MidLine
 from constants import field_width, FieldPos, drivetrain_length_with_bumpers
 import math
-from typing import Tuple
 
-coord = Tuple[meters, meters, radians]
-waypoints = Tuple[meters, meters]
-path = Tuple[coord, waypoints, coord]
+coord = (meters, meters, radians)
+waypoints = (meters, meters)
+path = (coord, waypoints, coord)
 
 initial: coord = (1.9 - drivetrain_length_with_bumpers/2, field_width-5.373, math.pi)
 
@@ -43,14 +42,14 @@ come_back_to_shoot_second_ring: path = (
     Field.POI.Coordinates.Notes.MidLine.kFarRight.withOffset(Translation2d(-4.3, -.7)),
 )
 
-get_third_ring: path = (
-    come_back_to_shoot_second_ring[2],
-    [],
-    Field.POI.Coordinates.Notes.MidLine.kCenter,
-)
+# get_third_ring: path = (
+#     come_back_to_shoot_second_ring[2],
+#     [],
+#     Field.POI.Coordinates.Notes.MidLine.kCenter,
+# )
 
-come_back_to_shoot_third_ring: path = (
-    get_third_ring[2],
-    [(FieldPos.Stage.stage_x, FieldPos.Stage.stage_y)],
-    Field.POI.Coordinates.Notes.MidLine.kCenter.withOffset(Translation2d(-4.5, -1.2)),
-)
+# come_back_to_shoot_third_ring: path = (
+#     get_third_ring[2],
+#     [(FieldPos.Stage.stage_x, FieldPos.Stage.stage_y)],
+#     Field.POI.Coordinates.Notes.MidLine.kCenter.withOffset(Translation2d(-4.5, -1.2)),
+# )
