ef17f2464704745e3e70044090630bbc334602a3
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-03-13 16:31:57 -0500

    Update elevator.py

--- subsystem/elevator.py
+++ subsystem/elevator.py
@@ -84,8 +84,13 @@ class Elevator(Subsystem):
         Climb down with feed forward
         """
         
-        self.target_length = 0
-        self.motor_extend.set_target_position(0, config.elevator_climb_ff, 1)
+        length = -0.06
+        
+        if length < -0.1:
+            length = -0.1
+        
+        self.target_length = length #the only time we should ever set the target length to a negative value
+        self.motor_extend.set_target_position(length, config.elevator_climb_ff, 1)
 
     def get_length(self) -> float:
         """
