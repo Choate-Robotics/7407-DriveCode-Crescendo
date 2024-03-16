ebbcde9025b4002eb8cc3c159bb4f16c70adc2e3
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-03-15 22:23:08 -0500

    Update can_optimizations.py

--- utils/can_optimizations.py
+++ utils/can_optimizations.py
@@ -72,6 +72,8 @@ class RevPeriodicFrames:
     
 max_period_rev = 32767
 
+optimized_basic_period_rev = 15
+
 def maximize_frame_period_rev(s: SparkMax, frame: RevPeriodicFrames):
     s.motor.setPeriodicFramePeriod(frame, max_period_rev)
 
@@ -82,8 +84,17 @@ def optimize_normal_sparkmax(s: SparkMax):
     maximize_frame_period_rev(s, RevPeriodicFrames.k5)
     maximize_frame_period_rev(s, RevPeriodicFrames.k6)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k0, 10)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, 20)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, 20)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, optimized_basic_period_rev)
+    
+def optimize_sparkmax_analog_sensor(s: SparkMax):
+    maximize_frame_period_rev(s, RevPeriodicFrames.k4)
+    maximize_frame_period_rev(s, RevPeriodicFrames.k5)
+    maximize_frame_period_rev(s, RevPeriodicFrames.k6)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k0, 10)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k3, 20)
     
 
 def optimize_sparkmax_absolute_encoder(s: SparkMax):
@@ -91,16 +102,16 @@ def optimize_sparkmax_absolute_encoder(s: SparkMax):
     maximize_frame_period_rev(s, RevPeriodicFrames.k4)
     maximize_frame_period_rev(s, RevPeriodicFrames.k6)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k0, 10)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, 15)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, 15)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k5, 100)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k5, 50)
     
 def optimize_sparkmax_absolute_encoder_all(s: SparkMax):
     maximize_frame_period_rev(s, RevPeriodicFrames.k3)
     maximize_frame_period_rev(s, RevPeriodicFrames.k4)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k0, 10)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, 15)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, 15)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, optimized_basic_period_rev)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k2, optimized_basic_period_rev)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k5, 100)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k6, 100)
     
@@ -111,5 +122,5 @@ def optimize_sparkmax_no_position(s: SparkMax):
     maximize_frame_period_rev(s, RevPeriodicFrames.k5)
     maximize_frame_period_rev(s, RevPeriodicFrames.k6)
     s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k0, 10)
-    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, 15)
+    s.motor.setPeriodicFramePeriod(RevPeriodicFrames.k1, optimized_basic_period_rev)
     
