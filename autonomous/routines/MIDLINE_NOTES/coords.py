34dbdefc4f6745fe78c6616df26e0ff9fcfa7c5e
e-bauman <ebauman26@choate.edu>
2024-03-23 08:04:49 -0400

    updated source side auto

--- autonomous/routines/MIDLINE_NOTES/coords.py
+++ autonomous/routines/MIDLINE_NOTES/coords.py
@@ -3,7 +3,7 @@ from robot_systems import Field
 from utils import POIPose
 from wpimath.geometry import Translation2d, Translation3d
 # from constants.FieldPos import MidLine
-from constants import field_width, FieldPos, drivetrain_length_with_bumpers
+from constants import field_width, FieldPos, drivetrain_length_with_bumpers, drivetrain_length
 import math
 
 coord = (meters, meters, radians)
@@ -21,7 +21,7 @@ shoot_first_note = (
 get_second_note = (
     shoot_first_note[2].withRotation(-155),
     [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.75, 0)),],
-    Field.POI.Coordinates.Notes.MidLine.kFarRight
+    Field.POI.Coordinates.Notes.MidLine.kFarRight.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, -0.5))
 )
 
 shoot_second_note = (
@@ -32,20 +32,20 @@ shoot_second_note = (
 
 get_third_note = (
     shoot_second_note[2].withRotation(-135),
-    [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.75, 0)),
+    [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.25, 0)),
     #  Field.POI.Coordinates.Notes.MidLine.kMidRight.withOffset(Translation2d(-1, 0))
      ],
-    Field.POI.Coordinates.Notes.MidLine.kMidRight
+    Field.POI.Coordinates.Notes.MidLine.kMidRight.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, -0.5))
 )
 
 shoot_third_note = (
     get_third_note[2],
-    [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.75, 0))],
+    [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.25, 0))],
     Field.POI.Coordinates.Waypoints.Auto.kMidlineAutoScoring.withRotation(135)
 )
 
 get_fourth_note = (
     shoot_third_note[2].withRotation(135),
     [Field.POI.Coordinates.Structures.Obstacles.kStage.withOffset(Translation2d(0.25, 0.25))],
-    Field.POI.Coordinates.Notes.MidLine.kCenter
+    Field.POI.Coordinates.Notes.MidLine.kCenter.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, 0))
 )
