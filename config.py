f9d9333b9a6e1b6e519a387d41623a4d4e6954f4
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-03-27 13:55:55 -0400

    tuning drivetrain

--- config.py
+++ config.py
@@ -228,9 +228,9 @@ back_right_encoder_zeroed_pos = 0.151 if comp_bot.get() else 0.984
 driver_centric: bool = True
 drivetrain_reversed: bool = False
 
-drivetrain_rotation_P: float = 9
+drivetrain_rotation_P: float = 4
 drivetrain_rotation_I: float = 0.0
-drivetrain_rotation_D: float = 0.08
+drivetrain_rotation_D: float = 0.1
 drivetrain_aiming_max_angular_speed: radians = 50#constants.drivetrain_max_angular_vel
 drivetrain_aiming_max_angular_accel: radians = 35 #constants.drivetrain_max_angular_accel
 
