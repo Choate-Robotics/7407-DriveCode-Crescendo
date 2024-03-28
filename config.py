b17e93c203033924e2584bf54e0e125a0d045444
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-03-28 18:01:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -276,7 +276,7 @@ odometry_distance_deviation_threshold: meters = 0.5
 odometry_std_auto_formula = lambda x: abs(x**2) / 2.5  # noqa
 odometry_std_tele_formula = lambda x: abs(x**1.3) / 1.3  # noqa
 odometry_crash_detection_enabled:bool = False
-odometry_crash_accel_threshold:float = 2.5 #G's
+odometry_crash_accel_threshold:float = 3.5 #G's
 
 
 #object detection
