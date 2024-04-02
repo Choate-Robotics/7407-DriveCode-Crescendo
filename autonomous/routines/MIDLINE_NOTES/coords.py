0f4055c6f0b2fea0a942f2b154f3214c3d9793a2
e-bauman <ebauman26@choate.edu>
2024-04-01 22:21:53 -0400

    new midline auto paths (still needs work)

--- autonomous/routines/MIDLINE_NOTES/coords.py
+++ autonomous/routines/MIDLINE_NOTES/coords.py
@@ -21,7 +21,7 @@ shoot_first_note = (
 get_second_note = (
     shoot_first_note[2].withRotation(-155),
     [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.75, 0)),],
-    Field.POI.Coordinates.Notes.MidLine.kFarRight.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, -0.5))
+    Field.POI.Coordinates.Notes.MidLine.kFarRight.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, 0))
 )
 
 shoot_second_note = (
@@ -35,17 +35,17 @@ get_third_note = (
     [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.25, 0)),
     #  Field.POI.Coordinates.Notes.MidLine.kMidRight.withOffset(Translation2d(-1, 0))
      ],
-    Field.POI.Coordinates.Notes.MidLine.kMidRight.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, -0.35))
+    Field.POI.Coordinates.Notes.MidLine.kMidRight.withOffset(Translation2d(0, 0)).withRotation(90)
 )
 
 shoot_third_note = (
-    get_third_note[2],
-    [Field.POI.Coordinates.Structures.Obstacles.kStageRightPost.withOffset(Translation3d(0, 1.5, 0))],
-    Field.POI.Coordinates.Waypoints.Auto.kMidlineAutoScoring.withRotation(-135)
+    get_third_note[2].withRotation(-90),
+    [Field.POI.Coordinates.Structures.Stage.kCenter.withOffset(Translation2d(0, 0.2))],
+    Field.POI.Coordinates.Structures.Stage.kLeft.withRotation(-135)
 )
 
 get_fourth_note = (
-    shoot_third_note[2].withRotation(135),
-    [Field.POI.Coordinates.Structures.Obstacles.kStage.withOffset(Translation2d(0.25, 0.25))],
+    shoot_third_note[2],
+    [],
     Field.POI.Coordinates.Notes.MidLine.kCenter.withOffset(Translation2d((-2 * drivetrain_length / 3) + 0.7, 0))
 )
