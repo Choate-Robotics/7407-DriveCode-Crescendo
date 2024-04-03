92853a7d5160b265b5178effe8c8f2c70f1516d6
e-bauman <ebauman26@choate.edu>
2024-04-03 15:19:53 -0400

    test auto

--- autonomous/routines/DRIVE_STRAIGHT/auto.py
+++ autonomous/routines/DRIVE_STRAIGHT/auto.py
@@ -3,6 +3,7 @@ from command.autonomous.trajectory import CustomTrajectory
 from robot_systems import Robot
 import constants
 import math
+import config
 
 from commands2 import (
     InstantCommand,
@@ -12,7 +13,7 @@ from commands2 import (
 from autonomous.auto_routine import AutoRoutine
 from autonomous.routines.DRIVE_STRAIGHT.coords import (
     drive_forward,
-    drive_forward_2,
+    # drive_forward_2,
     initial,
 )
 
@@ -28,33 +29,33 @@ path_1 = FollowPathCustom(
         start_pose=Pose2d(*drive_forward[0]),
         waypoints=[Translation2d(*coord) for coord in drive_forward[1]],
         end_pose=Pose2d(*drive_forward[2]),
-        max_velocity=vel,
-        max_accel=accel,
+        max_velocity=config.drivetrain_max_vel_auto,
+        max_accel=config.drivetrain_max_accel_auto - 1.5,
         start_velocity=0,
-        end_velocity=3,
-        rev=True,
-    ),
-    theta_f=math.radians(180)
-)
-
-path_2 = FollowPathCustom(
-    subsystem=Robot.drivetrain,
-    trajectory=CustomTrajectory(
-        start_pose=Pose2d(*drive_forward_2[0]),
-        waypoints=[Translation2d(*coord) for coord in drive_forward_2[1]],
-        end_pose=Pose2d(*drive_forward_2[2]),
-        max_velocity=vel,
-        max_accel=3,
-        start_velocity=3,
         end_velocity=0,
         rev=True,
     ),
     theta_f=math.radians(180)
 )
 
+# path_2 = FollowPathCustom(
+#     subsystem=Robot.drivetrain,
+#     trajectory=CustomTrajectory(
+#         start_pose=Pose2d(*drive_forward_2[0]),
+#         waypoints=[Translation2d(*coord) for coord in drive_forward_2[1]],
+#         end_pose=Pose2d(*drive_forward_2[2]),
+#         max_velocity=vel,
+#         max_accel=3,
+#         start_velocity=3,
+#         end_velocity=0,
+#         rev=True,
+#     ),
+#     theta_f=math.radians(180)
+# )
+
 auto = SequentialCommandGroup(
     path_1,
-    path_2
+    # path_2
 )
 
 routine = AutoRoutine(Pose2d(*initial), auto)
--- autonomous/routines/DRIVE_STRAIGHT/coords.py
+++ autonomous/routines/DRIVE_STRAIGHT/coords.py
@@ -1,4 +1,4 @@
-from units.SI import meters, radians
+from units.SI import meters, radians, feet_to_meters
 import constants
 import math
 
@@ -11,13 +11,13 @@ initial = (constants.field_length/2, constants.field_width/2, math.radians(180))
 drive_forward = (
     initial,
     [],
-    (initial[0] + 4, initial[1], math.radians(180)),
+    (initial[0] + 10*feet_to_meters, initial[1], math.radians(180)),
 )
 
-drive_forward_2 = (
-    drive_forward[2],
-    [],
-    (initial[0] + 7, initial[1], math.radians(180))
-)
+# drive_forward_2 = (
+#     drive_forward[2],
+#     [],
+#     (initial[0] + 7, initial[1], math.radians(180))
+# )
 
 
