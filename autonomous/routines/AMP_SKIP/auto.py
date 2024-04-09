64a52d0eedf7db158c3f958c9f8d1664f91e1165
Ann Song <gmsong1978@gmail.com>
2024-04-09 18:06:30 -0400

    Initial pos for amp skip

---
+++ autonomous/routines/AMP_SKIP/auto.py
@@ -0,0 +1,51 @@
+from command.autonomous.custom_pathing import FollowPathCustom, AngleType
+from command.autonomous.trajectory import CustomTrajectory, PoseType
+from robot_systems import Robot, Field
+from utils import POIPose
+from command import *
+import config
+import math
+
+from commands2 import (
+    InstantCommand,
+    SequentialCommandGroup,
+    ParallelCommandGroup,
+    ParallelDeadlineGroup,
+    WaitCommand
+)
+
+from autonomous.auto_routine import AutoRoutine
+from autonomous.routines.AMP_SKIP.coords import (
+    get_second_note,
+    get_third_note,
+    initial
+)
+
+from wpimath.geometry import Pose2d, Translation2d
+
+path_1 = FollowPathCustom(
+    subsystem=Robot.drivetrain,
+    trajectory=CustomTrajectory(
+        start_pose=get_second_note[0],
+        waypoints=[coord for coord in get_second_note[1]],
+        end_pose=get_second_note[2],
+        max_velocity=config.drivetrain_max_vel_auto,
+        max_accel=config.drivetrain_max_accel_auto - 1.5,
+        start_velocity=0,
+        end_velocity=0,
+        rev=True
+    ),
+    theta_f=math.radians(-180)
+)
+
+auto = SequentialCommandGroup(
+
+    path_1,
+    # path_4,
+    # path_5,
+    # path_2,
+    # path_3
+
+)
+
+routine = AutoRoutine(Pose2d(*initial), auto)
