cfc1fa600b26155ada7724708a04b4d9dc509896
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-04-10 17:51:20 -0400

    Update robot.py

--- robot.py
+++ robot.py
@@ -222,12 +222,12 @@ class _Robot(wpilib.TimedRobot):
                 command.DriveSwerveCustom(Robot.drivetrain),
             )
         )
-        # self.scheduler.schedule(
-        #     command.DeployIntake(Robot.intake).andThen(command.IntakeIdle(Robot.intake))
-        # )
         self.scheduler.schedule(
-            command.IntakeIdle(Robot.intake)
+            command.DeployIntake(Robot.intake).andThen(command.IntakeIdle(Robot.intake))
         )
+        # self.scheduler.schedule(
+        #     command.IntakeIdle(Robot.intake)
+        # )
 
         if Robot.wrist.note_in_feeder():
             states.flywheel_state = states.FlywheelState.shooting
