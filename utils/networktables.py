650625d3fd676e1b050bbcc47a4548050d45fb4d
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-04-13 10:51:20 -0400

    networktable manager and subtables

--- utils/networktables.py
+++ utils/networktables.py
@@ -1,8 +1,18 @@
+from __future__ import annotations
 import ntcore
 from enum import Enum
 
+class NetworkTableManager:
+    
+    def __init__(self):
+        self.nt = ntcore.NetworkTableInstance.getDefault()
+        
+    def getTable(self, table_name: str) -> NetworkTable:
+        table = self.nt.getTable(table_name)
+        return NetworkTable(table)
 
-class NetworkTables:
+
+class NetworkTable:
     
     class TopicType(Enum):
         DOUBLE = 0
@@ -12,105 +22,118 @@ class NetworkTables:
         BOOLEAN_ARRAY = 4
         DOUBLE_ARRAY = 5
         STRING_ARRAY = 6
-    
-    def __init__(self, table_name: str):
-        self.nt = ntcore.NetworkTableInstance.getDefault()
-        self.table = self.nt.getTable(table_name)
-        self.entries: dict[str,
+        
+    entries: dict[str,
             ntcore.BooleanArrayEntry | ntcore.BooleanEntry | ntcore.RawEntry | ntcore.DoubleEntry | ntcore.DoubleArrayEntry
             ] = {}
+    
+    def __init__(self, table_name: str | ntcore.NetworkTable):
+        if isinstance(table_name, str):
+            
+            self.table = ntcore.NetworkTableInstance.getDefault().getTable(table_name)
+        elif isinstance(table_name, ntcore.NetworkTable):
+            self.table = table_name
+        else:
+            raise ValueError("Invalid table_name type")
         
     def getNumber(self, key: str, default: float = 0.0) -> float:
         
-        entry:ntcore.DoubleEntry = self.__get_entry(key, NetworkTables.TopicType.DOUBLE, default)
+        entry:ntcore.DoubleEntry = self.__get_entry(key, NetworkTable.TopicType.DOUBLE, default)
         
         return entry.get(default)
     
     def getString(self, key: str, default: str = "") -> str:
         
-        entry:ntcore.StringEntry = self.__get_entry(key, NetworkTables.TopicType.STRING, default)
+        entry:ntcore.StringEntry = self.__get_entry(key, NetworkTable.TopicType.STRING, default)
         
         return entry.get(default)
     
     def getBoolean(self, key: str, default: bool = False) -> bool:
         
-        entry:ntcore.BooleanEntry = self.__get_entry(key, NetworkTables.TopicType.BOOLEAN, default)
+        entry:ntcore.BooleanEntry = self.__get_entry(key, NetworkTable.TopicType.BOOLEAN, default)
         
         return entry.get(default)
     
     def getRaw(self, key: str, default: bytes = b"") -> bytes:
         
-        entry:ntcore.RawEntry = self.__get_entry(key, NetworkTables.TopicType.RAW, default)
+        entry:ntcore.RawEntry = self.__get_entry(key, NetworkTable.TopicType.RAW, default)
         
         return entry.get(default)
     
     def getBooleanArray(self, key: str, default: list[bool] = []) -> list[bool]:
         
-        entry:ntcore.BooleanArrayEntry = self.__get_entry(key, NetworkTables.TopicType.BOOLEAN_ARRAY, default)
+        entry:ntcore.BooleanArrayEntry = self.__get_entry(key, NetworkTable.TopicType.BOOLEAN_ARRAY, default)
         
         return entry.get(default)
     
     def getNumberArray(self, key: str, default: list[float] = []) -> list[float]:
         
-        entry:ntcore.DoubleArrayEntry = self.__get_entry(key, NetworkTables.TopicType.DOUBLE_ARRAY, default)
+        entry:ntcore.DoubleArrayEntry = self.__get_entry(key, NetworkTable.TopicType.DOUBLE_ARRAY, default)
         
         return entry.get(default)
     
     def getStringArray(self, key: str, default: list[str] = []) -> list[str]:
         
-        entry:ntcore.StringArrayEntry = self.__get_entry(key, NetworkTables.TopicType.STRING_ARRAY, default)
+        entry:ntcore.StringArrayEntry = self.__get_entry(key, NetworkTable.TopicType.STRING_ARRAY, default)
         
         return entry.get(default)
     
     def putNumber(self, key: str, value: float):
         
-        entry:ntcore.DoubleEntry = self.__get_entry(key, NetworkTables.TopicType.DOUBLE, value)
+        entry:ntcore.DoubleEntry = self.__get_entry(key, NetworkTable.TopicType.DOUBLE, value)
         
         entry.set(value)
         
     def putString(self, key: str, value: str):
         
-        entry:ntcore.StringEntry = self.__get_entry(key, NetworkTables.TopicType.STRING, value)
+        entry:ntcore.StringEntry = self.__get_entry(key, NetworkTable.TopicType.STRING, value)
         
         entry.set(value)
         
     def putBoolean(self, key: str, value: bool):
         
-        entry:ntcore.BooleanEntry = self.__get_entry(key, NetworkTables.TopicType.BOOLEAN, value)
+        entry:ntcore.BooleanEntry = self.__get_entry(key, NetworkTable.TopicType.BOOLEAN, value)
         
         entry.set(value)
         
     def putRaw(self, key: str, value: bytes):
         
-        entry:ntcore.RawEntry = self.__get_entry(key, NetworkTables.TopicType.RAW, value)
+        entry:ntcore.RawEntry = self.__get_entry(key, NetworkTable.TopicType.RAW, value)
         
         entry.set(value)
         
     def putBooleanArray(self, key: str, value: list[bool]):
         
-        entry:ntcore.BooleanArrayEntry = self.__get_entry(key, NetworkTables.TopicType.BOOLEAN_ARRAY, value)
+        entry:ntcore.BooleanArrayEntry = self.__get_entry(key, NetworkTable.TopicType.BOOLEAN_ARRAY, value)
         
         entry.set(value)
         
     def putNumberArray(self, key: str, value: list[float]):
         
-        entry:ntcore.DoubleArrayEntry = self.__get_entry(key, NetworkTables.TopicType.DOUBLE_ARRAY, value)
+        entry:ntcore.DoubleArrayEntry = self.__get_entry(key, NetworkTable.TopicType.DOUBLE_ARRAY, value)
         
         entry.set(value)
         
     def putStringArray(self, key: str, value: list[str]):
         
-        entry:ntcore.StringArrayEntry = self.__get_entry(key, NetworkTables.TopicType.STRING_ARRAY, value)
+        entry:ntcore.StringArrayEntry = self.__get_entry(key, NetworkTable.TopicType.STRING_ARRAY, value)
         
         entry.set(value)
+        
+    def getSubTable(self, key: str) -> NetworkTable:
+
+        return NetworkTable(self.table.getSubTable(key))    
+    
         
     def __get_entry(self, key: str, type: TopicType, default: any):
         
         entry = None
         
-        try:
-            entry = self.entries[key]
-        except KeyError:
+        entry = self.entries.get(key)
+        
+        if entry is None:
+            print("Creating new entry", key, type, default)
+            print('entries size', len(self.entries))
             entry = self.__create_entry(key, type, default)
         
         return entry
@@ -121,27 +144,28 @@ class NetworkTables:
         topic = None
         
         match type:
-            case NetworkTables.TopicType.DOUBLE:
+            case NetworkTable.TopicType.DOUBLE:
                 topic = self.table.getDoubleTopic(key)
-            case NetworkTables.TopicType.STRING:
+            case NetworkTable.TopicType.STRING:
                 topic = self.table.getStringTopic(key)
-            case NetworkTables.TopicType.BOOLEAN:
+            case NetworkTable.TopicType.BOOLEAN:
                 topic = self.table.getBooleanTopic(key)
-            case NetworkTables.TopicType.RAW:
+            case NetworkTable.TopicType.RAW:
                 topic = self.table.getRawTopic(key)
-            case NetworkTables.TopicType.BOOLEAN_ARRAY:
+            case NetworkTable.TopicType.BOOLEAN_ARRAY:
                 topic = self.table.getBooleanArrayTopic(key)
-            case NetworkTables.TopicType.DOUBLE_ARRAY:
+            case NetworkTable.TopicType.DOUBLE_ARRAY:
                 topic = self.table.getDoubleArrayTopic(key)
-            case NetworkTables.TopicType.STRING_ARRAY:
+            case NetworkTable.TopicType.STRING_ARRAY:
                 topic = self.table.getStringArrayTopic(key)
             case _:
                 raise ValueError("Invalid TopicType")
         
         entry = topic.getEntry(default)
         
-        self.topics[key] = entry
+        self.entries[key] = entry
         
         return entry
+    
     
     
