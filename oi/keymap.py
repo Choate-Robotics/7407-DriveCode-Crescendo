4296e0cb72ce0aa0c5d589e40a00d7d23a32dfe0
Ericxiyinyang <ericxiyin@gmail.com>
2024-12-13 17:53:37 -0500

    push keymap

--- oi/keymap.py
+++ oi/keymap.py
@@ -34,7 +34,7 @@ class Keymap:
         X_MODE = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.X
         )
-        
+
     class Elevator:
         ELEVATOR_HIGH = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.Y
@@ -45,30 +45,28 @@ class Keymap:
         ELEVATOR_LOW = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.A
         )
-        
-        
+
         AMP = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.X
         )
+
     class Intake:
-        INTAKE_IN = commands2.button.Trigger(
-            lambda: Controllers.OPERATOR_CONTROLLER.getRawAxis(-controllerOPERATOR.RT) > 0.4
-        )
-        
         # INTAKE_IN = commands2.button.Trigger(
-        #     lambda: Controllers.DRIVER_CONTROLLER.getRawAxis(-controllerDRIVER.LT) > 0.4
+        #     lambda: Controllers.OPERATOR_CONTROLLER.getRawAxis(-controllerOPERATOR.RT) > 0.4
         # )
-        
-        
-        INTAKE_OUT = commands2.button.Trigger(
-            lambda: Controllers.OPERATOR_CONTROLLER.getRawAxis(-controllerOPERATOR.LT) > 0.4
-        )
-        
-        AUTO_INTAKE = commands2.button.Trigger(
+
+        INTAKE_IN = commands2.button.Trigger(
             lambda: Controllers.DRIVER_CONTROLLER.getRawAxis(-controllerDRIVER.LT) > 0.4
         )
-        
-        
+
+        # INTAKE_OUT = commands2.button.Trigger(
+        #     lambda: Controllers.OPERATOR_CONTROLLER.getRawAxis(-controllerOPERATOR.LT) > 0.4
+        # )
+
+        # AUTO_INTAKE = commands2.button.Trigger(
+        #     lambda: Controllers.DRIVER_CONTROLLER.getRawAxis(-controllerDRIVER.LT) > 0.4
+        # )
+
     class Shooter:
         ...
         AIM = commands2.button.Trigger(
@@ -78,37 +76,35 @@ class Keymap:
         # AIM_DRIVETRAIN_RIGHT = commands2.button.Trigger(
         #     lambda: Controllers.DRIVER_CONTROLLER.getRawAxis(-controllerDRIVER.LT) > 0.4
         # )
-        
+
         STATIC_FEED_SHOT = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.LB
         )
-        
-        SET_WRIST_SUBWOOFER = commands2.button.Trigger(
-            lambda: Controllers.OPERATOR_CONTROLLER.getPOV() == 90
-        )
-        
+
+        # SET_WRIST_SUBWOOFER = commands2.button.Trigger(
+        #     lambda: Controllers.OPERATOR_CONTROLLER.getPOV() == 90
+        # )
+
         ENABLE_AIM_WRIST_OPERATOR = commands2.button.Trigger(
             lambda: Controllers.OPERATOR_CONTROLLER.getPOV() == 270
         )
-        
+
         FLYWHEEL_MANUAL_REVERSE = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.R_3
         )
-        
+
         AMP = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.RB
         )
-        
-        FEED_SHOT = commands2.button.JoystickButton(
-            Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.B
-        )
-        
+
+        # FEED_SHOT = commands2.button.JoystickButton(
+        #     Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.B
+        # )
+
         # FEED_MIDLINE = commands2.button.JoystickButton(
         #     Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.START
         # )
-        
-        
-        
+
     class Feeder:
         FEED = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.RB
@@ -116,45 +112,44 @@ class Keymap:
         UNFEED = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.LB
         )
-        
+
         DUMP_NOTE = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.A
         )
-        
+
         # CLEAR_NOTE =
-        
+
         FEED_NOTE_FLYWHEEL = commands2.button.Trigger(
             lambda: Controllers.OPERATOR_CONTROLLER.getRawAxis(-controllerOPERATOR.L_JOY[1]) > 0.4
         )
-        
+
         SOURCE_FEED = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.START
         )
         # UNJAM =
-         
+
     class Climb:
-        
         CLIMB_UP = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.OPERATOR], controllerOPERATOR.SELECT
         )
-        
+
         CLIMB_DOWN = commands2.button.JoystickButton(
             Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.START
         )
-        
-        TRAP =  commands2.button.Trigger(
+
+        TRAP = commands2.button.Trigger(
             lambda: Controllers.OPERATOR_CONTROLLER.getPOV() == 0
         )
-        
+
         UNDO_CLIMB_UP = commands2.button.Trigger(
             lambda: Controllers.OPERATOR_CONTROLLER.getPOV() == 180
         )
-        
+
         # LOCK_STAGE_SOURCE = commands2.button.JoystickButton(
         #     Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.X
         # )
-        
+
         # LOCK_STAGE_AMP = commands2.button.JoystickButton(
         #     Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.Y
         # )
-        
+
